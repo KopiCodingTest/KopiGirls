cbd09850d4a452ee2331e3b5df3eabad32f93750
HAE2NI <hae0_02ni@ewhain.net>
2023-05-16 10:09:26 +0900

    변경

--- problems/week3_SORT/혜인/가장 큰 수.js
+++ problems/week3_SORT/혜인/가장 큰 수.js
@@ -1,10 +1,8 @@
 function solution(numbers) {
-  let answer = numbers
+  return numbers
     .map((number) => number.toString())
     .sort((a, b) => b + a - (a + b))
     .join("");
 
   return answer;
 }
-
-solution([3, 30, 34, 5, 9]);
